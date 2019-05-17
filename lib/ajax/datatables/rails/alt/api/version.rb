3dd67f7bf4e6b1e89748b33fda0b53890c5fc87b
Sean McCleary <sean@tryfleet.com>
2019-05-17 15:32:31 -0700

    Initial bundle gem generator

---
+++ lib/ajax/datatables/rails/alt/api/version.rb
@@ -0,0 +1,11 @@
+module Ajax
+  module Datatables
+    module Rails
+      module Alt
+        module Api
+          VERSION = "0.1.0"
+        end
+      end
+    end
+  end
+end
---
+++ spec/ajax/datatables/rails/alt/api_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Ajax::Datatables::Rails::Alt::Api do
+  it "has a version number" do
+    expect(Ajax::Datatables::Rails::Alt::Api::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
